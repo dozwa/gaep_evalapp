7f2a4f0729fbc89a0fa125e3ff915fed2112c751
Dorian Zwanzig <48753315+dozwa@users.noreply.github.com>
2024-06-25 16:49:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,11 @@ def get_sorted_answersegments(answer_id):
     answersegments = get_data(f"SELECT * FROM ANSWERSEGMENTS WHERE answer_id = '{str(answer_id)}'")
     return sorted(answersegments, key=lambda x: x[2])
 
+@st.cache_data(ttl=600)
+def get_question_text(question_id):
+    question_text = get_data(f"SELECT question FROM QUESTIONS WHERE question_id = '{str(question_id)}'")
+    return question_text[0][0]
+
 # Streamlit-State initialisieren
 def initialize_state():
     if 'dataset_index' not in st.session_state:
@@ -90,7 +95,7 @@ def render_reviewer_info():
     # Prüfer ID validieren
     global reviewer_id
     reviewer_id = st.sidebar.text_input("Prüfer ID:", key="prüfer_id")
-    allowed_ids = st.secrets["reviewer_ids"]
+    allowed_ids = ["6452","8640","3224","6511"]
     if reviewer_id not in allowed_ids:
         st.session_state['error_message'] = "Ungültige Prüfer ID. Bitte wählen Sie eine gültige ID aus der Liste."
     else:
@@ -110,24 +115,36 @@ def render_reviewer_info():
     
     st.sidebar.header("Prüfanweisungen")
     st.sidebar.markdown("Bitte bewerten Sie die Aussagen und geben Sie die Anzahl der relevanten Referenzen an.")
-    st.sidebar.markdown("Die Bewertung erfolgt auf einer Skala von 0 bis 4, wobei 0 für 'nicht relevant' und 4 für 'sehr relevant' steht.")
-    st.sidebar.markdown("Die Anzahl der relevanten Referenzen sollte als Zahl eingegeben werden.")
+    st.sidebar.markdown("Die Bewertung erfolgt auf einer Skala von 0 bis 4,")
+    st.sidebar.markdown("""
+    * 0 – Inhalt der Groundtruth ist gar nicht durch die Antwortsegmente abgebildet
+    * 1 – Inhalt der Groundtruth ist nur wenig durch die Antwortsegmente abgebildet
+    * 2 – Inhalt der Groundtruth ist einigermaßen durch die Antwortsegmente abgebildet
+    * 3 – Inhalt der Groundtruth ist nicht zum großen Teil durch die Antwortsegmente abgebildet
+    * 4 – Inhalt der Groundtruth ist vollständig durch die Antwortsegmente abgebildet""")
+    st.sidebar.markdown("Zusätzlich sollen für jede Groundtruth-Aussage die relevanten Aussagen aus den Antwortsegmenten angegeben, die zur Abdeckung beigetragen haben. Bitte referenzieren Sie die Antwortsegmente mit ihren Indizes. Trennen Sie diese mit einem Komma. Beispiel: `1,3,5`")
 
 # Hauptinhalt rendern
 def render_main_content():
     current_index = st.session_state['dataset_index']
     st.header(f"Datensatz {current_index + 1} von {len(qna_ids_list)}")
+    st.write(f"Frage-ID: {qna_ids_list[current_index][1]}, Answer-ID: {qna_ids_list[current_index][0]}")
+    st.write(get_question_text(qna_ids_list[current_index][1]))
     
     col1, col2 = st.columns([1, 1])
     with col1:
-        st.header("Groundtruth")
+        st.header("MI-Antwort (Menschliche Intelligenz)")
         sorted_groundtruths = get_sorted_groundtruths(qna_ids_list[current_index][1])
+        st.write(len(sorted_groundtruths), "Aussagen gefunden.")
         evaluation = []
         additional_texts = []
         for i, groundtruth in enumerate(sorted_groundtruths):
             sub_col1, sub_col2, sub_col3 = st.columns([3, 1, 2])
             with sub_col1:
-                st.write(groundtruth[3], groundtruth[4])  # 3 = Index, 4 = Text
+                if len(sorted_groundtruths) > 1:
+                    st.write(groundtruth[3], groundtruth[4])  # 3 = Index, 4 = Text
+                else:
+                    st.write("Auf Basis der Leitlinie konnte keine Antwort gegeben werden.")
             with sub_col2:
                 eval_value = st.selectbox(f'Bewertung', range(5), key=f"eval_{current_index}_{i}")
                 evaluation.append(eval_value)
@@ -142,8 +159,9 @@ def render_main_content():
         st.session_state['additional_texts'][current_index] = additional_texts
 
     with col2:
-        st.header("Referenzsätze")
+        st.header("KI-Antwort (Künstliche Intelligenz)")
         sorted_answersegments = get_sorted_answersegments(qna_ids_list[current_index][0])
+        st.write(len(sorted_answersegments), "Aussagen gefunden.")
         for answer in sorted_answersegments:
             st.write(answer[2], answer[3])  # 2 = Index, 3 = Text
 
@@ -152,6 +170,7 @@ st.set_page_config(layout="wide")
 
 # Datenabfrage
 qna_ids_query = "SELECT answer_id, question_id FROM ANSWERS"
+
 qna_ids_list = get_data(qna_ids_query)
 
 # Initialisierung des States
